acf5c4fecf3c23b24a30e42543e0579f2a7bb801
MICAELA RICCARDI <47574710@ort>
2023-05-18 08:08:23 -0300

    terminamos

--- Models/Info.cs
+++ Models/Info.cs
@@ -4,27 +4,27 @@ static class Info
     
     static void InicializarLista()
     {
-        DateTime fechaIn = new DateTime(9/7/1810); 
+        DateTime fechaIn = new DateTime(1810, 07, 09); 
         Pais argentina = new Pais("Argentina", "banderaArg.png", 45810000,  fechaIn, "Obelisco");
         listaPaises.Add(argentina);
         
-        fechaIn = new DateTime(30/01/1648);
+        fechaIn = new DateTime(1648, 01, 30);
         Pais holanda = new Pais("Holanda", "banderaHola.png", 17530000, fechaIn,"Casa de Ana Frank");
         listaPaises.Add(holanda);
 
-        fechaIn = new DateTime(14/05/1948);
+        fechaIn = new DateTime(1948, 05, 14);
         Pais israel = new Pais("Israel", "banderaIsra.jpg", 9367000, fechaIn,"El Kotel");
         listaPaises.Add(israel);
 
-        fechaIn = new DateTime(22/08/1849);
+        fechaIn = new DateTime(1849, 08, 22);
         Pais italia = new Pais("Italia", "banderaItalia.png", 58800000, fechaIn,"Coliseo de Roma");
         listaPaises.Add(italia);
 
-        fechaIn = new DateTime(27/09/1821);
+        fechaIn = new DateTime(1821, 09, 27);
         Pais mexico = new Pais("México", "banderaMex.png", 126705138, fechaIn,"Chichén Itzá");
         listaPaises.Add(mexico);
 
-        fechaIn = new DateTime(31/05/1961);
+        fechaIn = new DateTime(1961, 05, 31);
         Pais sudafrica = new Pais("Sudafrica", "banderaSuda.png", 60604000, fechaIn,"Parque Nacional Kruger");
         listaPaises.Add(sudafrica);
     }
